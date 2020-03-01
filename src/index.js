8d443467df2b3cf309977dd8fbadeac6bb5548cf
svdfsdev <58512937+svdfsdev@users.noreply.github.com>
2020-03-01 19:40:07 +0300

    feat: added implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n > 0) {
+        return +String(n).split('').reverse().join('');
+    }
+    else {
+        return +String(n).slice(1).split('').reverse().join('');
+    }
 }
